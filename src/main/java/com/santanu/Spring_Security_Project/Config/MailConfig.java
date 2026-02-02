5c72c1ec073cbc687a4cb2247eab5f550c035c66
SANTANU NANDI <santanu2005nandi@gmail.com>
2026-02-02 17:25:00 +0530

    Create MailConfig.java

---
+++ src/main/java/com/santanu/Spring_Security_Project/Config/MailConfig.java
@@ -0,0 +1,31 @@
+package com.santanu.Spring_Security_Project.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(Environment env) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(env.getProperty("MAIL_HOST"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("MAIL_PORT", "587")));
+        mailSender.setUsername(env.getProperty("MAIL_USERNAME"));
+        mailSender.setPassword(env.getProperty("MAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+
+        return mailSender;
+    }
+}
